2b209cd6113dbc490c4cb3283c17d06dca1b14b7
mobina ezzoddin <m.ezzoddin@karboom.io>
2022-08-15 20:17:07 +0430

    get url input console and create short link

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mezz.com/url-shortner
 
 go 1.18
 
-require github.com/lib/pq v1.10.6
+require (
+	github.com/itchyny/base58-go v0.2.0
+	github.com/lib/pq v1.10.6
+)
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha256"
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"github.com/itchyny/base58-go"
+	"net/url"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -21,9 +27,53 @@ type ShortUrl struct {
 var database *sql.DB
 
 func main() {
+	baseUrl := getConsoleUrl()
+	validateUrl(baseUrl)
+
+	id := insert(baseUrl)
+	idAsString := fmt.Sprint(id)
+	generateShortLink(baseUrl.url, idAsString)
 	connectDB()
-	var sampleUrl = ShortUrl{url: "https://mezz.ir", createdAt: time.Now()}
-	insert(sampleUrl)
+}
+
+func getConsoleUrl() ShortUrl {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter url: ")
+	url, _ := reader.ReadString('\n')
+	url = strings.TrimSuffix(url, "\n")
+
+	return ShortUrl{url: url}
+}
+
+func validateUrl(inputUrl ShortUrl) {
+	_, err := url.ParseRequestURI(inputUrl.url)
+	checkErr(err)
+}
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return string(encoded)
+}
+
+func generateShortLink(baseUrl string, id string) string {
+	urlHashBytes := sha256Of(baseUrl + id)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+
+	return finalString[:8]
 }
 
 func connectDB() {
